d9ae0cda3a7829b0d064f80e36eda6efcaa86b21
Noothan am <100135382+Noothan-am@users.noreply.github.com>
2023-10-08 00:03:54 +0530

    add scss

--- client/src/pages/ErrorPage.tsx
+++ client/src/pages/ErrorPage.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-
+import "../styles/error.scss";
 function ErrorPage() {
   return (
     <>
-      <div className="error-page">
-        <div className="error-page__heading">Error</div>
-        <div className="error-page__error-message">404 Page Not Found</div>
+      <div className="App">
+        <div className="container">
+          <h1>404 Page Not Found</h1>
+          <p>It seems that page you are looking for no longer exists.</p>
+          <button>Back Homepage</button>
+        </div>
       </div>
     </>
   );
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,40 +1,65 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+// import "../styles/login.css";
+// const Login = () => {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+
+//   const handleLoginSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+//     event.preventDefault();
+//     console.log(`Email: ${email}, Password: ${password}`);
+//   };
+
+//   return (
+//     <div className="login">
+//       <h1 className="login__heading">Welocme Back!</h1>
+//       <form onSubmit={handleLoginSubmit} className="login__form">
+//         <div className="login__email">
+//           Email:
+//           <input
+//             type="email"
+//             value={email}
+//             className="login__input-field"
+//             onChange={(event) => setEmail(event.target.value)}
+//           />
+//         </div>
+//         <div className="login__input">
+//           Password:
+//           <input
+//             type="password"
+//             value={password}
+//             className="login__input-field"
+//             onChange={(event) => setPassword(event.target.value)}
+//           />
+//         </div>
+//         <button type="submit" className="login__button">
+//           Submit
+//         </button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default Login;
+
+import React from "react";
+import "../styles/login.scss";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleLoginSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(`Email: ${email}, Password: ${password}`);
-  };
-
   return (
-    <div className="login">
-      <h1 className="login__heading">Login</h1>
-      <form onSubmit={handleLoginSubmit} className="login__form">
-        <div className="login__email">
-          Email:
-          <input
-            type="email"
-            value={email}
-            className="login__input-field"
-            onChange={(event) => setEmail(event.target.value)}
-          />
-        </div>
-        <div className="login__input">
-          Password:
-          <input
-            type="password"
-            value={password}
-            className="login__input-field"
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </div>
-        <button type="submit" className="login__button">
-          Submit
-        </button>
-      </form>
+    <div className="login-container">
+      <h1 className="login-title">Welcome Back!</h1>
+      <div className="login-form">
+        <input
+          type="email"
+          className="login-input"
+          placeholder="Email address"
+        />
+        <input type="password" className="login-input" placeholder="Password" />
+        <button className="login-button">Login</button>
+        <a href="/forgot-password" className="login-link">
+          Forgot Password?
+        </a>
+      </div>
     </div>
   );
 };
