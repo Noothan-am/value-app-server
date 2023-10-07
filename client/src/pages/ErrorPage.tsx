9b2ed0cfcf66c8f8321462e1c6fb436ce9aa2dba
Noothan am <100135382+Noothan-am@users.noreply.github.com>
2023-10-07 23:28:09 +0530

    add css

--- client/src/pages/ErrorPage.tsx
+++ client/src/pages/ErrorPage.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function ErrorPage() {
-  return <div>ErrorPage</div>;
+  return (
+    <>
+      <div className="error-page">
+        <div className="error-page__heading">Error</div>
+        <div className="error-page__error-message">404 Page Not Found</div>
+      </div>
+    </>
+  );
 }
 
 export default ErrorPage;
