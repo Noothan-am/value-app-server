c0cfd3eb225ea7f27279d6ba05ec9fe5f46db0b6
Noothan am <100135382+Noothan-am@users.noreply.github.com>
2023-10-08 20:55:37 +0530

    login page design finished

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,65 +1,46 @@
-// import React, { useState } from "react";
-// import "../styles/login.css";
-// const Login = () => {
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-
-//   const handleLoginSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-//     event.preventDefault();
-//     console.log(`Email: ${email}, Password: ${password}`);
-//   };
-
-//   return (
-//     <div className="login">
-//       <h1 className="login__heading">Welocme Back!</h1>
-//       <form onSubmit={handleLoginSubmit} className="login__form">
-//         <div className="login__email">
-//           Email:
-//           <input
-//             type="email"
-//             value={email}
-//             className="login__input-field"
-//             onChange={(event) => setEmail(event.target.value)}
-//           />
-//         </div>
-//         <div className="login__input">
-//           Password:
-//           <input
-//             type="password"
-//             value={password}
-//             className="login__input-field"
-//             onChange={(event) => setPassword(event.target.value)}
-//           />
-//         </div>
-//         <button type="submit" className="login__button">
-//           Submit
-//         </button>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default Login;
-
-import React from "react";
+import React, { useState } from "react";
+import { BiLockAlt } from "react-icons/bi";
 import "../styles/login.scss";
 
 const Login = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleLoginSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(`Email: ${email}, Password: ${password}`);
+  };
+
   return (
-    <div className="login-container">
-      <h1 className="login-title">Welcome Back!</h1>
-      <div className="login-form">
-        <input
-          type="email"
-          className="login-input"
-          placeholder="Email address"
-        />
-        <input type="password" className="login-input" placeholder="Password" />
-        <button className="login-button">Login</button>
-        <a href="/forgot-password" className="login-link">
+    <div className="login">
+      <h1 className="login__title">Welcome Back!</h1>
+      <form className="login__form" onSubmit={handleLoginSubmit}>
+        <div className="login__form-email">
+          <input
+            type="email"
+            className="login__form-email-input"
+            placeholder="Email address"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </div>
+        <div className="login__form-password">
+          <input
+            type="password"
+            className="login__form-password-input"
+            placeholder="Password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <BiLockAlt className="login__form-password-lock" />
+        </div>
+        <button type="submit" className="login__form-button">
+          Login
+        </button>
+        <a href="/forgot-password" className="login__form-link">
           Forgot Password?
         </a>
-      </div>
+      </form>
     </div>
   );
 };
