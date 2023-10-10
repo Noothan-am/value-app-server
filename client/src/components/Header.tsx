0cdba6c7b1a74202e38619e363ec95b8f1afa6b3
Noothan am <100135382+Noothan-am@users.noreply.github.com>
2023-10-10 21:59:20 +0530

    header setup

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { IoIosArrowBack } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
+const styles = require("../styles/header.module.scss").default;
+
+function Header() {
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className={styles["header"]}>
+        <div className={styles["header__container"]}>
+          <div className={styles["header__container-logo"]}>
+            <img src={require("../assets/images/Group 26943.png")} alt="" />
+          </div>
+          <div className={styles["header__container-goback"]}>
+            <div>
+              <IoIosArrowBack
+                onClick={() => navigate("/home")}
+                className={styles["header__container-goback-sidearrow"]}
+              />
+            </div>
+            <div>
+              <a
+                href="/home"
+                className={styles["header__container-goback-text"]}
+              >
+                Profile
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
---
+++ client/src/pages/Self.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/Header";
+
+function Self() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default Self;
