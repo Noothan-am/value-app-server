674dc19d7d08f2392ed105414ea92f62c7a72752
noothan-am <noothanam13@gmail.com>
2024-04-12 12:31:53 +0530

    fixes error

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 8000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "*",
+    origin: process.env.CLIENT_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
     maxAge: 3600 * 24,
   })
